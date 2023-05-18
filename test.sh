5c5d92fce9fc2eefb4e17e106e811cfe948eadda
romulortr <romulortr@gmail.com>
2023-05-18 16:57:40 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {1..10} ; do
     n=`expr $i \* 1`
-        for j in {299..1} ; do            
+        for j in {299..0} ; do            
             # run the test
             python3 run.py --world_idx $j
             sleep 5
